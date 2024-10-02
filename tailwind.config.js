0c30b60460fd2370f3545e704ea36c6657cd37ec
marcin-lew <128682826+marcin-lew@users.noreply.github.com>
2024-10-02 20:44:59 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,36 @@ module.exports = {
                 'grid-pattern': "linear-gradient(to bottom, theme('colors.neutral.950 / 0%'), theme('colors.neutral.950 / 100%')), url('/images/noise.png')"
             },
             colors: {
-                neutral: colors.neutral
+                neutral: colors.neutral,
+                white: '#FFFFFF',
+                lightGrey1: '#EFF0F3',
+                lightGrey2: '#E4E5E9',
+                grey: '#C0C0C0',
+                darkGrey: '#9A9494',
+                black: '#2B2C34',
+                blue: '#6246EA',
+                background: '#E5E5E5', // Dodajemy kolor tła
             },
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans]
-            }
+                sans: ['"Plus Jakarta Sans"', ...defaultTheme.fontFamily.sans],
+            },
+            fontSize: {
+                h1: '64px',
+                h2: '24px',
+                h3: '20px',
+                p1: '20px',
+                p2: '18px',
+                p3: '16px',
+                p4: '14px',
+            },
+            fontWeight: {
+                regular: '400',
+                semibold: '600',
+                bold: '700',
+            },
+            backgroundColor: {
+                default: '#E5E5E5', // Kolor tła jako domyślny
+            },
         }
     },
     daisyui: {
